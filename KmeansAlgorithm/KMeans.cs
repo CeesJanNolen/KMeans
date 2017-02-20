6df5bf75316a3c9ea4c1c1d1bb45f00e22ba3acb
Cees-Jan <ceesjannolen@live.nl>
2017-02-20 18:36:57 +0100

    kmeans initialization

---
+++ KmeansAlgorithm/KMeans.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KmeansAlgorithm
+{
+    public class KMeans
+    {
+        public List<GenericVector> Dataset;
+        public int Iterations;
+        public int Clusters;
+        public Dictionary<int, GenericVector> Centroids;
+        private readonly Random _random = new Random();
+
+
+        public void Run()
+        {
+            Centroids = GenerateRandomCentroids(Clusters);
+            //for loop Iterations
+            for (var i = 0; i < Iterations; i++)
+            {
+                var oldCluster = Dataset.Select(v => v.Cluster).ToList();
+
+                //assign data set
+                AssignDataset();
+
+                //recalculate clusters
+                RecalculateCenteroids();
+
+                if (!ClustersChanged(oldCluster, Dataset.Select(v => v.Cluster).ToList()))
+                {
+
+                    Console.WriteLine(i);
+                    break;
+                }
+//                    break;
+            }
+        }
+
+        public void AssignDataset()
+        {
+            Dataset.ForEach(vector => vector.Cluster = GetNearestCluster(vector));
+            foreach (var vector in Dataset)
+            {
+                vector.Cluster = GetNearestCluster(vector);
+            }
+        }
+
+        public int GetNearestCluster(GenericVector vector)
+        {
+            var clusterid = Centroids
+                .OrderBy(v => GenericVector.Distance(vector, v.Value))
+                .Select(v => v.Key)
+                .FirstOrDefault();
+            return clusterid;
+        }
+
+        public Dictionary<int, GenericVector> GenerateRandomCentroids(int clusters)
+        {
+            var centroids = new Dictionary<int, GenericVector>();
+            var index = 0;
+            clusters.Times(() => centroids.Add(index++, RandomVector()));
+            return centroids;
+        }
+
+        public bool ClustersChanged(List<int> a, List<int> b)
+        {
+            return a.Where((value, index) => value != b[index]).Any();
+        }
+
+        public void RecalculateCenteroids()
+        {
+            foreach (var centroid in Centroids.Keys.ToList())
+            {
+                var cluster = Dataset.Where(v => v.Cluster == centroid).ToList();
+
+                Centroids[centroid] = cluster
+                    .Aggregate(new GenericVector(Dataset.First().Size),
+                        (current, y) => current.Sum(y))
+                    .Devide(cluster.Count);
+            }
+        }
+
+        public GenericVector RandomVector()
+        {
+            return Dataset[_random.Next(Dataset.Count)];
+        }
+
+        public void PrintClusters()
+        {
+            var clusters = Dataset.GroupBy(v => v.Cluster);
+            foreach (var cluster in clusters)
+            {
+                Console.WriteLine("Cluster " + cluster.ElementAt(0).Cluster + " has " + cluster.Count());
+            }
+        }
+    }
+}
---
+++ KmeansAlgorithm/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.IO;
+using System.Linq;
+
+namespace KmeansAlgorithm
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+//            var x = new List<int>();
+//            x.Add(6);
+//            x.Add(1);
+//            x.Add(7);
+//            x.Add(1);
+//            x.Add(4);
+//            x.Add(4);
+//            x.Add(1);
+//            x.Add(1);
+//            x.Add(6);
+//            x.Add(1);
+//            x.Add(4);
+//            x.Add(3);
+//            x.Add(6);
+//            x.Add(7);
+//            x.Add(8);
+//            x.Add(5);
+//
+//            x = x.OrderByDescending(v => v).ToList();
+//
+//            foreach (var i in x)
+//            {
+//                Console.Write(i);
+//            }
+
+
+            //genericvector ophalen en extra var geven voor welke cluster hij zit.
+            //aantal clusters bepalen
+            //aantal iterations neerzetten
+            //dataset maken -> lijst met genericvector
+            //bovenstaande items doogeven aan kmeans
+            //kmeans starten
+
+
+            //KMEANS
+            //random x aantal clusters maken //generate cluster
+            //items aan cluster toevoegen    //assing vectors to clusters
+            //bovenstaand itereren totdat of iteraties opzijn of de clusters veranderen niet meer. //check iteration
+
+
+/*
+VARIABLES
+int clusters
+int iterations
+set dataset
+list centroids
+
+FUNCTIONS
+generate random centroids
+run functie
+recalc centroids
+setchanged(datasetold, datasetnew)
+getnearestcluster(vector)
+
+*/
+            var dataDictionary = new Dictionary<int, GenericVector>();
+
+            var lines = File.ReadAllLines("WineData.csv").Select(v => v.Split(',').Select(float.Parse).ToList()).ToList();
+
+            for (var i = 0; i < lines.Count; i++)
+            {
+                for (var j = 0; j < lines[i].Count; j++)
+                {
+                    if (!dataDictionary.ContainsKey(j))
+                        dataDictionary[j] = new GenericVector();
+                    dataDictionary[j].Add(lines[i][j]);
+                }
+            }
+
+            var kMeans = new KMeans
+            {
+                Iterations = 100,
+                Dataset = dataDictionary.Values.ToList(),
+                Clusters = 4
+            };
+
+            kMeans.Run();
+            kMeans.PrintClusters();
+
+        }
+    }
+}
